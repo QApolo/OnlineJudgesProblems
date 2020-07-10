3c9384a98ccbb47395536ddcdb45501295ac8145
Juan Carlos García Medina <Carlos_isc7@outlook.com>
2020-07-10 11:16:06 -0500

    solved while listening eternal youth

---
+++ Codeforces_Ladders/A_SystemofEquations.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    int counter = 0;
+    for(int a = 0; a < 1000; a++) {
+        if ( a * (a*a*a - 2*n*a+1) - m + n*n == 0 && n - a*a >= 0) {
+            //cout << a << " " << n - a*a << endl;
+            counter++;
+        }
+    }
+    cout << counter << endl;
+}
